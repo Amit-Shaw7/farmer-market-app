4c395fe8c30ca2997240deec1ae202862766f7c6
Amit-Shaw7 <maahiamit777@gmail.com>
2024-02-01 16:37:55 +0530

    basic funtionality done not tested

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -26,7 +26,7 @@ router.get(
     "/cart",
     verifyToken,
     getCart
-)
+);
 
 
 
--- backend/src/routes/otp.routes.js
+++ backend/src/routes/otp.routes.js
@@ -2,14 +2,21 @@ import express from "express";
 import validateOtp from "../controllers/otp/validateOtp.controller.js";
 import validateData from "../middlewares/validateData.middleware.js";
 import ValidateOtpSchema from "../schemas/otp/validateOtp.schema.js";
+import ResendOtpSchema from "../schemas/otp/resendOtp.schema.js";
+import resendOtp from "../controllers/otp/resendOtp.controller.js";
 
 const router = express.Router();
-
 
 router.post(
     "/validate/:userId",
     validateData(ValidateOtpSchema),
     validateOtp
+);
+
+router.put(
+    "/resend/:userId",
+    validateData(ResendOtpSchema),
+    resendOtp
 )
 
 export default router;
